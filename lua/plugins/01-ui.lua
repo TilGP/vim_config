6cd1cd3cb1690ffe00dec9bd4bf0530f856cc11b
TilGP <tilpockrandt@gmail.com>
2023-11-29 20:33:43 +0100

    moved some plugin definitions around

--- lua/plugins/01-ui.lua
+++ lua/plugins/01-ui.lua
@@ -1,1 +1,25 @@
-return {}
+return {
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, "😄")
+    end,
+  },
+}
--- lua/plugins/05-vim-cmake.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "cdelledonne/vim-cmake",
-  keys = {
-    { "<Leader>cg", "<cmd>CMakeGenerate<cr>", desc = "Generate build system" },
-    { "<Leader>cb", "<cmd>CMakeBuild<cr>", desc = "Build a project" },
-  },
-}
