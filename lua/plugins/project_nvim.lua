d1fe8d6fc47c8796fe3f57240c9a4734f19a95d8
Til Georg Pockrandt <til.pockrandt@check24.de>
2024-02-05 11:06:29 +0100

    idk

---
+++ lua/plugins/project_nvim.lua
@@ -0,0 +1,20 @@
+return {
+  "ahmedkhalf/project.nvim",
+  opts = {
+    manual_mode = false,
+    silent_chdir = false,
+    detection_methods = { "lsp", "pattern" },
+    patterns = { ".git", ".project", ".project.lua", ".project.toml", "=app" },
+    scope_chdir = "global",
+  },
+  event = "VeryLazy",
+  config = function(_, opts)
+    require("project_nvim").setup(opts)
+    require("lazyvim.util").on_load("telescope.nvim", function()
+      require("telescope").load_extension("projects")
+    end)
+  end,
+  keys = {
+    { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+  },
+}
