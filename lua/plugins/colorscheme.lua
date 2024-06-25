d045cb0192efcd6ac936787138072cb07dda0b79
TilGP <til.pockrandt@check24.de>
2024-06-25 12:55:04 +0200

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -42,9 +42,20 @@ return {
     },
   },
   {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 100,
+    config = function()
+      vim.g.nightflyTerminalColors = false
+      vim.g.nightflyVirtualTextColor = true
+      vim.g.nightflyWinSeparator = 2
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "nightfly",
     },
   },
 }
