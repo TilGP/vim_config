4b9da545532127bddc71e2ae2de13ebe2d42ecf3
TilGP <til.pockrandt@check24.de>
2024-06-26 16:58:01 +0200

    added highlighting for embedded sql inside python

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,4 +1,8 @@
+local script_dir = vim.fn.expand("%:p")
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = { ensure_installed = { "cpp" } },
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = { "cpp", "python", "sql" },
+    },
 }
