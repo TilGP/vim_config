584c6df34d53d180abf0b5628a3cbb23ac96cc0d
TilGP <til.pockrandt@check24.de>
2024-06-26 13:09:42 +0200

    idk

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 
 vim.api.nvim_set_keymap("n", "tn", ":Telescope notify<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F5>", ":DapContinue<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<F8>", ":DapStepOver<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<F7>", ":DapStepInto<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<F6>", ":DapTerminate<CR>", { noremap = true, silent = false })
