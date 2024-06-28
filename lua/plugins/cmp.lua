31256dc7df84668231d0db82b056550f7c1a160b
TilGP <til.pockrandt@check24.de>
2024-06-28 13:11:44 +0200

    custom config for nvim-cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,79 @@
+return {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+    opts = function()
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        local cmp = require("cmp")
+        local defaults = require("cmp.config.default")()
+        local auto_select = true
+        return {
+            auto_brackets = {}, -- configure any filetype to auto add brackets
+            completion = {
+                completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+            },
+            preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+                ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+                ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-CR>"] = function(fallback)
+                    cmp.abort()
+                    fallback()
+                end,
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "path" },
+            }, {
+                { name = "buffer" },
+            }),
+            formatting = {
+                format = function(_, item)
+                    local icons = LazyVim.config.icons.kinds
+                    if icons[item.kind] then
+                        item.kind = icons[item.kind] .. item.kind
+                    end
+                    return item
+                end,
+            },
+            experimental = {
+                ghost_text = {
+                    hl_group = "CmpGhostText",
+                },
+            },
+            sorting = defaults.sorting,
+
+            enabled = function()
+                -- disable completion in comments
+                local context = require("cmp.config.context")
+                -- keep command mode completion enabled when cursor is in a comment
+                if vim.api.nvim_get_mode().mode == "c" then
+                    return true
+                elseif vim.bo.filetype == "markdown" then
+                    return false
+                else
+                    return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+                end
+            end,
+        }
+    end,
+    main = "lazyvim.util.cmp",
+}
