b63e3ab29670919361e60d41d6051ddeaf8c7bf0
TilGP <til.pockrandt@check24.de>
2024-06-28 13:13:29 +0200

    cmake_tools config

--- lua/plugins/cmake_tools.lua
+++ lua/plugins/cmake_tools.lua
@@ -1,3 +1,9 @@
+local function getNumberOfCores()
+    local handle = io.popen("nproc")
+    local result = handle:read("*a")
+    handle:close()
+    return tonumber(result)
+end
 return {
     "Civitasv/cmake-tools.nvim",
     config = function()
@@ -8,7 +14,7 @@ return {
             cmake_use_preset = true,
             cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
             cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
-            cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
+            cmake_build_options = { "-j " .. getNumberOfCores() - 2 }, -- this will be passed when invoke `CMakeBuild`
             -- support macro expansion:
             --       ${kit}
             --       ${kitGenerator}
@@ -21,7 +27,7 @@ return {
             end, -- this is used to specify generate directory for cmake, allows macro expansion, can be a string or a function returning the string, relative to cwd.
             cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
             cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
-            cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
+            cmake_kits_path = "~/CMakeKits.json", -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
             cmake_variants_message = {
                 short = { show = true }, -- whether to show short message
                 long = { show = true, max_length = 40 }, -- whether to show long message
