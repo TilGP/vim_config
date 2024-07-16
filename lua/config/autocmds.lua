4bf40153de288bbaf3bf5a94203a0f3973e18a09
Til Georg Pockrandt <til.pockrandt@check24.de>
2024-07-16 08:13:08 +0200

    added ansiesc plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Associate "Jeninsfile" with groovy filetype
 vim.cmd([[
   autocmd BufRead,BufNewFile Jenkinsfile set filetype=groovy
+]])
+
+-- Run :AnsiEsc on files with the .ansi ending
+vim.cmd([[
+  autocmd BufRead,BufNewFile *.ansi AnsiEsc
 ]])
