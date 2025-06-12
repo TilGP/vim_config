cfb0c1dd6f0e0976fa2a9a9e5e0d58813fde4ce1
TilGP <tilpockrandt@gmail.com>
2025-06-12 09:26:00 +0200

    removed unused groups

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -30,7 +30,7 @@ return {
       -- table: additional groups that should be cleared
       extra_groups = {},
       -- table: groups you don't want to clear
-      exclude_groups = { "SnacksBackdrop", "CursorLine", "NeoTreeExpander" },
+      exclude_groups = { "SnacksBackdrop", "CursorLine" },
       -- function: code to be executed after highlight groups are cleared
       -- Also the user event "TransparentClear" will be triggered
       on_clear = function() end,
